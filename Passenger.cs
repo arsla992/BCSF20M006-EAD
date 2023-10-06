4fb641671325f6bfd5378cc6dc2ea8a72afb1703
HAFIZ MUHAMMAD ARSLAN <100121931+arsla992@users.noreply.github.com>
2023-10-06 14:19:10 +0500

    Add files via upload

---
+++ Passenger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PassengerDetails
+{
+    public class Passenger
+    {
+        public string PassengerName;
+        public string PassengerPhoneNo;
+        public string giveRating()
+        {
+            string rating;
+            do
+            {
+                Console.Write("\nGive Rating (1 to 5): ");
+                rating = Console.ReadLine();
+            } while (rating != "1" && rating != "2" && rating != "3" && rating != "4" && rating != "5");
+
+            return rating;
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehicleDetails
+{
+    public class Vehicle
+    {
+        public String VehicleType { get; set; }
+        public String VehicleModel { get; set; }
+        public String VehicleLicensePlate { get; set; }
+    }
+}
+ 
